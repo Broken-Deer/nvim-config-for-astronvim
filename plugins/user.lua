cfc49266a30197665e6ed5e90615003b0e3eb0a9
Broken-Deer <kxscluabcbook@outlook.com>
2024-02-23 05:00:20 -0500

    feat(rust): debug or test rust program.

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,6 +9,9 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "simrat39/rust-tools.nvim",
+  },
   -- {
   --   "simrat39/rust-tools.nvim",
   --   config = function()
